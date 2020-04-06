8a3d5ff8e0cd2509e574c0d51ee4bc1f6dfb6d4d
dilfyg <barker.dill98@gmail.com>
2020-04-06 10:28:32 +0100

    changing to fasta

--- corona.py
+++ corona.py
@@ -1,3 +1,9 @@
-test
+# import packages
+import pandas as pd
+from Bio.Seq import Seq
 
+# load the sequence
 
+seq = Seq('C:/Users/barke/Documents/2020/Github-Repositories/Bioinformatics/genome.fasta')
+
+print(seq)
