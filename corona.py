a8737983c16cb94af194a665eb6b36f357bec629
dilfyg <barker.dill98@gmail.com>
2020-04-06 11:07:25 +0100

    bioseq stuff

--- corona.py
+++ corona.py
@@ -1,9 +1,19 @@
 # import packages
-import pandas as pd
 from Bio.Seq import Seq
+import os
+from Bio.Alphabet import generic_dna
+
+# set wd
+os.chdir('C:/Users/barke/Documents/2020/Github-Repositories/Bioinformatics')
 
 # load the sequence
+from Bio import SeqIO
+for record in SeqIO.parse("genome.fasta", "fasta"):
+    id  = record.id
+    seq = record.seq
 
-seq = Seq('C:/Users/barke/Documents/2020/Github-Repositories/Bioinformatics/genome.fasta')
-
-print(seq)
+# id gives the id of the genome, whilst seq is the fill sequence
+print(len(seq))
+# translate the sequence
+translated_seq = seq.translate()
+print(translated_seq)
