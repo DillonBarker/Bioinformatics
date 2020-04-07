1d331cc0c198e76158573a517c0f505f9394c4ab
dilfyg <barker.dill98@gmail.com>
2020-04-07 11:51:58 +0100

    Project is set up on my PC now I need to sort this out so that I can edit from my laptop.

--- corona.py
+++ corona.py
@@ -3,8 +3,10 @@ from Bio.Seq import Seq
 import os
 from Bio.Alphabet import generic_dna
 
-# set wd
-os.chdir('C:/Users/barke/Documents/2020/Github-Repositories/Bioinformatics')
+# set wd on laptop
+#os.chdir('C:/Users/barke/Documents/2020/Github-Repositories/Bioinformatics')
+#set wd on pc
+os.chdir('C:/Users/barke/Documents/2020/Coding and Stats/Github-Repositories/Bioinformatics')
 
 # load the sequence
 from Bio import SeqIO
@@ -13,7 +15,8 @@ for record in SeqIO.parse("genome.fasta", "fasta"):
     seq = record.seq
 
 # id gives the id of the genome, whilst seq is the fill sequence
-print(len(seq))
+
 # translate the sequence
 translated_seq = seq.translate()
-print(translated_seq)
+
+#
