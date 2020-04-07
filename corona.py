310029e00e631756eab7aa3c5187eb4a799e7117
dilfyg <barker.dill98@gmail.com>
2020-04-07 14:41:57 +0100

    work through on corona.py more

--- corona.py
+++ corona.py
@@ -23,26 +23,12 @@ genome_translated_seq = genome_seq.translate()
 for record in SeqIO.parse("spike.fasta", "fasta"):
     spike_id  = record.id
     spike_translated_seq = record.seq
-``` align the spike sequence to the genome sequence
-from Bio.Blast.Applications import NcbiblastpCommandline
-from io import StringIO
-#from StringIO import StringIO ## i dont know if I need this package, also says I dont have it.
-from Bio.Blast import NCBIXML
+
+# blast
+BLAST = "C:/Program Files/NCBI/blast-2.10.0+/bin/blastp.exe"
+OUTDIR = "C:/Users/barke/Documents/2020/Coding and Stats/Github-Repositories/Bioinformatics"
 
 
-# Run BLAST and parse the output as XML
-output = NcbiblastpCommandline(cmd ="C:/Program Files/NCBI/blast-2.10.0+/bin/blastp.exe", query="spike.fasta", subject="genome.fasta", outfmt=5)()[0]
-blast_result_record = NCBIXML.read(StringIO(output))
 
-# Print some information on the result
-for alignment in blast_result_record.alignments:
-    for hsp in alignment.hsps:
-        print('****Alignment****')
-        print('sequence:', alignment.title)
-        print('length:', alignment.length)
-        print('e value:', hsp.expect)
-        print(hsp.query)
-        print(hsp.match)
-        print(hsp.sbjct)```
 
 
